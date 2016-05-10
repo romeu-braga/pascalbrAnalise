1b883b5555f4990adfb85c07d31222695a1c029e
romeu-braga <romeu-braga@outlook.com>
2016-05-09 22:32:37 -0300

    Atualizando auxiliares

--- auxiliares.c
+++ auxiliares.c
@@ -1,53 +1,22 @@
 #include "cabecalho.h"
 
-/* lookup - uma função para processar operadores e parênteses e retornar o token */
-int lookup(char ch) {
-    switch(ch) {
-    case '(' :
-        addChar();
-        nextToken = LEFT_PAREN;
-        break;
-    case ')' :
-        addChar();
-        nextToken = RIGHT_PAREN;
-        break;
-    case '+' :
-        addChar();
-        nextToken = ADD_OP;
-        break;
-    case '-' :
-        addChar();
-        nextToken = SUB_OP;
-        break;
-    case '*' :
-        addChar();
-        nextToken = MULT_OP;
-        break;
-    case '/' :
-        addChar();
-        nextToken = DIV_OP;
-        break;
-    default :
-        addChar();
-        nextToken = EOF;
-        break;
-    }
-    return nextToken;
-}
-
-/* addChar - uma função para adicionar nextChar ao vetor lexeme */
 void addChar() {
     if(lexLen <= 98) {
         lexeme[lexLen++] = nextChar;
-        lexeme[lexLen] = 0; //DUVIDA
+        lexeme[lexLen] = 0;
     } else {
         printf("ERROR - lexema muito grande\n");
     }
 }
 
-/* getChar - uma função para obter o próximo caractere da entrada e determinar sua classe de caracteres */
 void getChar() {
     if((nextChar = getc(in_fp)) != EOF) {
+        if(nextChar == 10) {
+            linha = 0;
+            coluna = coluna + 1;
+        } else {
+            linha = linha + 1;
+        }
         if(isalpha(nextChar)) {
             charClass = LETTER;
         } else if (isdigit(nextChar)) {
@@ -56,14 +25,21 @@ void getChar() {
             charClass = UNKNOWN;
         }
     } else {
+        linha = linha + 1;
         charClass = EOF;
     }
 }
 
-
-/* getNonBlank - uma função para chamar getChar até que ela retorne um caractere diferente de espaço em branco */
 void getNonBlank() {
     while(isspace(nextChar)) {
         getChar();
+    }
+}
+
+int fimPalavraReservada() {
+    if(charClass == UNKNOWN || nextChar == EOF){
+        return 1;
+    } else {
+        return 0;
     }
 }
